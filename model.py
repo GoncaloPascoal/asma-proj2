bf3961c1973227b59db1d61232faeccc1ce963c7
JoaoASousa <joaosousa.pt@gmail.com>
2022-05-26 16:30:21 +0100

    model class begginings

--- model.py
+++ model.py
@@ -1,18 +1,45 @@
-from mesa import Agent
-
+from mesa import Agent, Model
+from mesa.space import MultiGrid
+from mesa.time import RandomActivation
 
 # NS = Natural Selection
 class NSAgent(Agent):
     """An agent with base attribute values"""
 
-    def __init__(self, unique_id: int, model: Model) -> None:
+    def __init__(self, unique_id: int, model: Model, speed = 1, awareness = 1, size = 1, trail = 0) -> None:
         super().__init__(unique_id, model)
-        self.speed = 1
-        self.awareness = 1 # or maybe name it "reach"
-        self.size = 1
-        self.trail = 0 # binary
+        self.speed = speed
+        self.awareness = awareness # or maybe name it "reach"
+        self.size = size
+        self.trail = trail # binary
+    
 
     def step(self):
-        # The agent's step will go here.
+        # Agent's step
         print("Hi, I'm agent " + str(self.unique_id) + ".")
 
+        
+
+
+
+class NSModel(Model):
+    """Model with a specified number of agents"""
+
+    def __init__(self, N: int, width: int, height: int) -> None:
+        self.num_agents = N
+        self.grid = MultiGrid(width, height, False)
+        self.schedule = RandomActivation(self)
+
+        # Create agents
+        for i in range(self.num_agents):
+            a = NSAgent(i, self)
+            self.schedule.add(a)
+
+            # Add each agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y)) # Adds the coordinate to the agent automatically
+    
+    def step(self):
+        """Advance the model by one step"""
+        self.schedule.step()
--- run.py
+++ run.py
@@ -1,1 +1,7 @@
 # Python script that will run the model when invoked via mesa runserver
+
+# temporary
+from model import NSModel
+
+empty_model = NSModel(10)
+empty_model.step()

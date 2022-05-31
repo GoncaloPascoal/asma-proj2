15d7b168183ac52ee6f062924c9d1d95265879f8
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-05-31 19:00:12 +0100

    Implement preliminary grid visualization

--- server.py
+++ server.py
@@ -1,1 +1,27 @@
-# Visualization support, including the server class
+
+from mesa import Agent
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+from model import Food, Organism, NSModel
+
+def agent_portrayal(agent: Agent):
+    portrayal = {}
+
+    if isinstance(agent, Food):
+        portrayal['Shape'] = 'circle'
+        portrayal['Color'] = 'green'
+        portrayal['Filled'] = 'true'
+        portrayal['Layer'] = 0
+
+    return portrayal
+
+grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+server = ModularServer(
+    NSModel,
+    [grid],
+    'Natural Selection Model',
+    {'num_agents': 10, 'width': 10, 'height': 10}
+)
+print(grid.render(server.model))
+server.launch()
+

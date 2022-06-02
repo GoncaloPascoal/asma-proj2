7ab8bb991371c2c1382a353a8f27408188180d58
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-06-02 21:04:10 +0100

    Organism attribute visualization

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 
+from matplotlib.colors import to_hex
+
 from mesa import Agent, Model
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.TextVisualization import TextData
+
 from model import Food, Organism, NSModel
 
 def generation(model: Model) -> str:
@@ -21,12 +24,21 @@ def agent_portrayal(agent: Agent):
         portrayal['xAlign'] = 0.2
         portrayal['yAlign'] = 0.2
     elif isinstance(agent, Organism):
+        w = 0.2 + 0.4 * (agent.size - Organism.MIN_SIZE) / (Organism.MAX_SIZE - Organism.MIN_SIZE)
+        h = 0.2 + 0.4 * agent.awareness / Organism.MAX_AWARENESS
+
+        speed_rate = (agent.speed - Organism.MIN_SPEED) / (Organism.MAX_SPEED - Organism.MIN_SPEED)
+
         portrayal['Shape'] = 'rect'
-        portrayal['Color'] = 'blue'
-        portrayal['w'] = 0.25
-        portrayal['h'] = 0.25
-        portrayal['xAlign'] = 0.8
-        portrayal['yAlign'] = 0.8
+        portrayal['Color'] = to_hex([
+            0.1 + 0.1 * speed_rate,
+            0.2 + 0.1 * speed_rate,
+            0.4 + 0.5 * speed_rate
+        ])
+        portrayal['w'] = w
+        portrayal['h'] = h
+        portrayal['xAlign'] = 0.9 - w / 2
+        portrayal['yAlign'] = 0.9 - h / 2
 
         for attr in ['speed', 'awareness', 'size', 'energy', 'prob_survival', 'prob_replication']:
             portrayal[attr] = agent.__dict__.get(attr)
